8cc96b2393c578eee4a93f7c69686671ed44ca5a
Ashish Tiwari <tashish1995@gmail.com>
2023-04-21 00:52:36 +0530

    Initial commit

---
+++ src/main/java/com/s3/controller/FileStorageController.java
@@ -0,0 +1,35 @@
+package com.s3.controller;
+
+import com.s3.service.FileStorageService;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class FileStorageController {
+
+    private FileStorageService service;
+
+    public FileStorageController(FileStorageService service) {
+        this.service = service;
+    }
+
+    @PostMapping("upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file){
+        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+    }
+
+    @GetMapping("download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName){
+        byte[] downloadFile = service.downloadFile(fileName);
+        ByteArrayResource byteArrayResource = new ByteArrayResource(downloadFile);
+        return ResponseEntity
+                .ok()
+                .contentLength(downloadFile.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition","attachment; filemame\""+fileName+"\"")
+                .body(byteArrayResource);
+    }
+}
